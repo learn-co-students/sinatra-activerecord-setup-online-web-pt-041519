78ce611dd0d22675eabba65c476a9ec920bb48e8
github username <github email address>
2019-08-14 17:00:49 +0900

    Done.

--- db/migrate/20190814075628_create_dogs.rb
+++ db/migrate/20190814075628_create_dogs.rb
@@ -1,12 +1,21 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def up
+  #def up
+   # create_table :dogs do |t|
+    #  t.string :name
+     # t.string :breed
+    #end
+  #end
+ 
+  #def down
+   # drop_table :dogs
+  #end
+
+  #refactor below! up & down and change have the same functionality
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
-  end
- 
-  def down
-    drop_table :dogs
+
   end
 end
